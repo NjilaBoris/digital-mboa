61320cbace4d1dc1bfa23b50f57afe1d78a7db32
NjilaBoris <njilaboris2@gmail.com>
2025-10-04 13:19:21 +0100

    Implement the rest of LogoTicker

---
+++ constants/index.tsx
@@ -0,0 +1,22 @@
+import { ChartLineIcon, Globe, Handshake } from "lucide-react";
+
+export const WhyChooseUs = [
+  {
+    icon: <Globe />,
+    title: "Expertise Locale & Globale",
+    description:
+      "Nous comprenons les besoins spécifiques du marché africain tout en intégrant les meilleures pratiques mondiales.",
+  },
+  {
+    icon: <Handshake />,
+    title: "Approche Personnalisée",
+    description:
+      "Chaque client est unique. Nous adaptons nos solutions pour répondre précisément à vos objectifs et défis.",
+  },
+  {
+    icon: <ChartLineIcon />,
+    title: "Résultats Mesurables",
+    description:
+      "Nous nous engageons à fournir des résultats tangibles qui stimulent la croissance et l'innovation.",
+  },
+];
